b5e277789dc2790fde6f3d9c79d9be9849cea282
Keith Devops <keith-ko@hotmail.com>
2020-12-08 06:12:55 +1100

    Add browserroute

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 import "./index.css";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
 import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
